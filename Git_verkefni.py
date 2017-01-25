5325b34224f214f29aee1fde9a88755afc6b4f20
pallsv227 <pallsv227@nemi.tskoli.is>
2017-01-25 11:13:32 +0000

    git python

---
+++ Git_verkefni.py
@@ -0,0 +1,6 @@
+#Páll Gunnar Svansson 25.1.17
+#dæmi 1
+tala_1 = int(input("sláðu inn eina tölu: "))
+tala_2 = int(input("sláðu inn aðra tölu: "))
+svar = ((tala_1)+(tala_2))
+print ("útkoman af þessum tölum er ", svar)
