13bd7f2708953bd945c98c72b15113afab2d8402
pallsv227 <pallig1@gmail.com>
2017-01-25 12:11:40 +0000

    daemi 3.5

--- Git_verkefni.py
+++ Git_verkefni.py
@@ -1,13 +1,29 @@
 #Páll Gunnar Svansson 25.1.17
+
 #dæmi 1
 tala_1 = int(input("sláðu inn eina tölu: "))
 tala_2 = int(input("sláðu inn aðra tölu: "))
 svar = ((tala_1)+(tala_2))
 print ("útkoman af þessum tölum er ", svar)
+
 #dæmi 2
 fornafn = input("sláðu inn fornafnið þitt: ")
 eftirnafn = input("sláðu inn eftirnafnið þitt: ")
 print ("halló", fornafn, eftirnafn)
+
 #dæmi 3
 texti = str(input("sláðu inn texta: "))
-print ("í þessum texta eru",(sum(1 for c in texti if c.isupper())), "Hástafir og", sum(1 for c in texti if c.islower()), "lágstafir")
+hastafir = 0
+lagstafir = 0
+eftirHastafi = 0
+for x in texti:
+    if x.isupper():
+        hastafir = hastafir +1
+    if x.islower():
+        lagstafir = lagstafir +1
+for x in range(len(texti)):
+    if texti[x].isupper():
+        i= x+1
+        if texti[i].lower():
+            eftirHastafi = eftirHastafi +1
+print ("í þessum texta eru", hastafir, "Hástafir,", lagstafir, "lágstafir og", eftirHastafi,"eftir stórum staf")
